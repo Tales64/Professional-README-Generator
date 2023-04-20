1f0f8700d2704f7246ca1579356c040313c8a013
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2023-04-19 23:50:21 -0700

    fixed the questions

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [{
+    type: 'input',
+    message: 'What is your app used for?',
+    name: 'app usage',
+  },
+  {
+    type: 'input',
+    message: 'How should your app be used?',
+    name: 'app usage',
+  },
+  {
+    type: 'input',
+    message: 'How do you install your app?',
+    name: 'app usage',
+  },
+  {
+    type: 'input',
+    message: 'How do you report issues?',
+    name: 'app usage',
+  },
+  {
+    type: 'input',
+    message: 'How do you make contributions?',
+    name: 'app usage',
+  },
+  {
+      type: 'checkbox',
+      message: 'What License does this app use?',
+      name: 'License',
+      choices: ['GNU AGPLv3', 'GNU GPLv3',
+      'GNU LGPLv3', 'Mozilla Public License 2.0',
+      'Apache License 2.0', 'MIT License', 'Boost Software License 1.0',
+      'The Unlicense'],
+    },
+    {type: 'input',
+name: 'username',
+message: 'What is your GitHub username?',}
+]
     
-}];
+;
 
 inquirer
   .prompt([
@@ -46,19 +85,25 @@ inquirer
       {type: 'input',
   name: 'username',
   message: 'What is your GitHub username?',}
-  ])
-  .then((response) =>
-    response.confirm === response.password
-      ? console.log('Success!')
-      : console.log('You forgot your password already?!')
-  );
+  ]) 
+  
 
 
-// TODO: Create a function to write README file
-fs.writeToFile(fileName, data) {}
+  function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        if (err) throw err;
+        console.log('File created!');
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
+// function to initialize program
+function init() {
+    inquirer.prompt(questions).then(function (data) {
+        writeToFile("README2.md", generateMarkdown(data))
+    });
+
+}
+
+// function call to initialize program
 init();
